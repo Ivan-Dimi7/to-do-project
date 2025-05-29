2bc047c1b859a0c73578344a06e42c9607a36111
Ivan-Dimi7 <ivan2dim@gmail.com>
2025-05-29 19:32:37 +0300

    moved the logic for managing tasks into a new component Task Groups and implemented adding/deleting task groups

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,42 @@
-import Task from './Task'
+import TaskGroup from './TaskGroup'
 import {useState} from "react";
+import Task from "./Task.jsx";
+import taskGroup from "./TaskGroup";
 
 
 function App() {
-    const [tasks, setTasks] = useState([])
-    const [taskTitle, setTaskTitle] = useState('')
+    const [taskGroups, setTaskGroups] = useState([])
 
-    function addTask() {
-        if (taskTitle.trim()) {
-            const newTask = { title: taskTitle };
-            setTasks(currTasks => [...currTasks, newTask]);
+    function addTaskGroup() {
+        const title = prompt('Enter title');
+        if (title.trim()) {
+            const newTaskGroup = { title: title };
+            setTaskGroups(currTasks => [...currTasks, newTaskGroup]);
         } else {
             alert("Please enter title");
         }
     }
 
-    function deleteTask(index) { //todo use IDs instead of indexes
+    function deleteTaskGroup(index) { //todo use IDs instead of indexes
         if(window.confirm("Are you sure you want to delete?")){
-            const newTasks = tasks.filter((_,i) => i !== index);
-            setTasks(newTasks);
+            const newTaskGroup = taskGroups.filter((_,i) => i !== index);
+            setTaskGroups(newTaskGroup);
         }
     }
-
-    function markAsDone(index) {
-        if (!tasks[index].done){
-            if(window.confirm("Once you mark tasks as done it cannot be undone! Are you sure?")){
-                const updatedTasks = [...tasks];
-                updatedTasks[index].done = true;
-                setTasks(updatedTasks);
-            }
-        }
-    }
-
-    function moveTaskUp(index) {
-        if(index > 0){
-            const newTasks = [...tasks];
-            [newTasks[index], newTasks[index - 1]] = [newTasks[index - 1], newTasks[index]];
-            setTasks(newTasks);
-        }
-    }
-
-    function moveTaskDown(index) {
-        if(index < tasks.length-1){
-            const newTasks = [...tasks];
-            [newTasks[index], newTasks[index + 1]] = [newTasks[index + 1], newTasks[index]];
-            setTasks(newTasks);
-        }
-    }
-
 
 
     return (
         <div>
             <h1>TO-DO LIST</h1>
-            <input onChange={(e) => setTaskTitle(e.target.value)} value={taskTitle}/>
-            <button onClick={addTask}>Add New Task</button>
-            <ol>
-                {tasks.map((task, index) => (
-                   <li>
+            {taskGroups.map((taskGroup, index) => (
                        <span>
-                           <Task key={index} title={task.title} />
-                           <input type="checkbox" checked={task.done} disabled={task.done} onChange={() => markAsDone(index)}/>
-                           <button onClick={() => deleteTask(index)}>Delete</button>
-                           <button onClick={() => moveTaskUp(index)}>UP</button>
-                           <button onClick={() => moveTaskDown(index)}>DOWN</button>
-
+                           <TaskGroup key={index} title={taskGroup.title} />
+                           <button onClick={() => deleteTaskGroup(index)}>Delete Task Group</button>
                        </span>
 
-                   </li>
-                ))}
+            ))}
+            <button onClick={addTaskGroup}>Add New Task Group</button>
 
-            </ol>
         </div>
     )
 }
---
+++ src/TaskGroup.jsx
@@ -0,0 +1,75 @@
+import Task from './Task'
+import {useState} from "react";
+
+function TaskGroup(props){
+    const [tasks, setTasks] = useState([])
+
+    function addTask() {
+        const title = prompt('Enter title');
+        if (title.trim()) {
+            const newTask = { title: title };
+            setTasks(currTasks => [...currTasks, newTask]);
+        } else {
+            alert("Please enter title");
+        }
+    }
+
+    function deleteTask(index) { //todo use IDs instead of indexes
+        if(window.confirm("Are you sure you want to delete?")){
+            const newTasks = tasks.filter((_,i) => i !== index);
+            setTasks(newTasks);
+        }
+    }
+
+    function markAsDone(index) {
+        if (!tasks[index].done){
+            if(window.confirm("Once you mark tasks as done it cannot be undone! Are you sure?")){
+                const updatedTasks = [...tasks];
+                updatedTasks[index].done = true;
+                setTasks(updatedTasks);
+            }
+        }
+    }
+
+    function moveTaskUp(index) {
+        if(index > 0){
+            const newTasks = [...tasks];
+            [newTasks[index], newTasks[index - 1]] = [newTasks[index - 1], newTasks[index]];
+            setTasks(newTasks);
+        }
+    }
+
+    function moveTaskDown(index) {
+        if(index < tasks.length-1){
+            const newTasks = [...tasks];
+            [newTasks[index], newTasks[index + 1]] = [newTasks[index + 1], newTasks[index]];
+            setTasks(newTasks);
+        }
+    }
+
+
+
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <button onClick={addTask}>Add New Task</button>
+            <ol>
+                {tasks.map((task, index) => (
+                    <li>
+                       <span>
+                           <Task key={index} title={task.title} />
+                           <input type="checkbox" checked={task.done} disabled={task.done} onChange={() => markAsDone(index)}/>
+                           <button onClick={() => deleteTask(index)}>Delete</button>
+                           <button onClick={() => moveTaskUp(index)}>UP</button>
+                           <button onClick={() => moveTaskDown(index)}>DOWN</button>
+
+                       </span>
+
+                    </li>
+                ))}
+
+            </ol>
+        </div>
+    )
+}
+export default TaskGroup;
