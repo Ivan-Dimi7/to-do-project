97c328352cb95ca1f3c690d8fdf4089260aebc74
Ivan-Dimi7 <ivan2dim@gmail.com>
2025-05-30 04:25:44 +0300

    conncted the add/delete logic for task gorups to the json as well

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,28 +8,43 @@ function Dashboard() {
         const title = prompt('Enter title');
 
         if (title.trim() && /^[a-zA-Z0-9\s]+$/.test(title)) {
-            const newTaskGroup = { title: title };
-            setTaskGroups(currTasks => [...currTasks, newTaskGroup]);
+            const newTaskGroup = { title: title, tasks: [] };
+
+            fetch("http://localhost:3001/taskGroups", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newTaskGroup)
+            })
+                .then(r => r.json())
+                .then(data => {
+                    setTaskGroups(taskGroups => [...taskGroups, data]); // add returned group with id
+                });
+
         } else {
             alert("Please enter a valid title");
         }
     }
 
-    function deleteTaskGroup(index) { //todo use IDs instead of indexes
-        if(window.confirm("Are you sure you want to delete?")){
-            const newTaskGroup = taskGroups.filter((_,i) => i !== index);
-            setTaskGroups(newTaskGroup);
+
+    function deleteTaskGroup(id) {
+        if(window.confirm("Are you sure you want to delete?")) {
+            fetch(`http://localhost:3001/taskGroups/${id}`, {
+                method: "DELETE"
+            }).then(() => {
+                setTaskGroups(currTaskGroups => currTaskGroups.filter(taskGroup => taskGroup.id !== id));
+            });
         }
     }
+
 
 
     return (
         <div>
             <h1>TO-DO LIST</h1>
-            {taskGroups.map((taskGroup, index) => (
+            {taskGroups.map((taskGroup) => (
                 <span>
-                           <TaskGroup key={index} title={taskGroup.title} />
-                           <button onClick={() => deleteTaskGroup(index)}>Delete Task Group</button>
+                           <TaskGroup key={taskGroup.id} title={taskGroup.title} />
+                           <button onClick={() => deleteTaskGroup(taskGroup.id)}>Delete Task Group</button>
                        </span>
 
             ))}
