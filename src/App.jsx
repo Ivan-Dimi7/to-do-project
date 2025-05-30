50ecc43da62f90cb011cb067d98c9adefc0c252a
Ivan-Dimi7 <ivan2dim@gmail.com>
2025-05-30 05:37:05 +0300

    added registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Dashboard from "./components/Dashboard"
+import Register from "./components/Register";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
     );
 }
---
+++ src/components/Register.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+function Register() {
+    const [username, setUsername] = useState('');
+    const [pass, setPass] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+
+    const registerUser = (e) => {
+        e.preventDefault();
+
+        if (pass !== confirmPass) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        fetch('http://localhost:3001/users?username=' + username)
+            .then(res => res.json())
+            .then(users => {
+                if (users.length === 0) {
+
+                    const newUser = { username, password: pass };
+                    fetch('http://localhost:3001/users', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(newUser)
+                    }).then(res => res.json())
+
+                } else {
+                    alert("Username already exists");
+                }
+            });
+    };
+
+    return (
+        <form onSubmit={registerUser}>
+            <h2>Register</h2>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+                required
+            />
+            <input
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPass}
+                onChange={(e) => setConfirmPass(e.target.value)}
+                required
+            />
+            <button type="submit">Register</button>
+        </form>
+    );
+}
+
+export default Register;
